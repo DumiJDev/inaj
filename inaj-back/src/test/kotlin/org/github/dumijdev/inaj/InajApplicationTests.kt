4a2b1203ccbeaf8a63555e1e82ff22cf97bc9c30
Dumildes Paulo <dumildesfernando@outlook.com>
2023-08-07 02:13:13 +0100

    add backend and frontend

---
+++ inaj-back/src/test/kotlin/org/github/dumijdev/inaj/InajApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.github.dumijdev.inaj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InajApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
